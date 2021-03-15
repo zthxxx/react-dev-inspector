bd846b5b0217fa9bef6b942529ee10be79b4ee3c
zthxxx <zthxxx.me@gmail.com>
2021-03-16 01:03:00 +0800

    ci: adjust ci env var

--- scripts/vercel-build.sh
+++ scripts/vercel-build.sh
@@ -4,6 +4,9 @@
 # https://www.explainshell.com/explain/1posix/set
 set -ex
 
+# https://github.com/chalk/supports-color/blob/main/index.js#L21
+export FORCE_COLOR=true
+
 env
 
 npm run build
@@ -12,8 +15,8 @@ npm run build
 rm -rf sites/cra/src/layouts
 cp -rf sites/umi3/src/layouts sites/cra/src/
 
-rm -rf sites/vite/src/layouts
-cp -rf sites/umi3/src/layouts sites/vite/src/
+rm -rf sites/vite2/src/layouts
+cp -rf sites/umi3/src/layouts sites/vite2/src/
 
 
 npm run build:site
