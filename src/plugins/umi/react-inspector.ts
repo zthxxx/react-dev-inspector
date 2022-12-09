e7af91ab6b152f29c4c1cdde0ca538beb676a048
GQ <tonny008@163.com>
2022-12-09 13:44:32 +0800

    more specific

--- src/plugins/umi/react-inspector.ts
+++ src/plugins/umi/react-inspector.ts
@@ -52,5 +52,5 @@ export default function inspectorPlugin(api: IApi) {
    * due to umi3 not use webpack devServer,
    * so need add launch editor middleware manually
    */
-  (api.addBeforeMiddlewares ?? api.addBeforeMiddewares).bind(this, createLaunchEditorMiddleware)
+  (api.addBeforeMiddlewares ?? api.addBeforeMiddewares).bind(api, createLaunchEditorMiddleware)
 }
