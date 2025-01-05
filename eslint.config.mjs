3292e226597e1bafc11bbeefeded416b02c05c58
zthxxx <zthxxx.me@gmail.com>
2025-01-05 13:33:00 +0800

    ci: fix pnpm version on CI building

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,7 +9,7 @@ import antfu from '@antfu/eslint-config'
  */
 export default antfu(
   {
-  // Type of the project. 'lib' for libraries, the default is 'app'
+    // Type of the project. 'lib' for libraries, the default is 'app'
     type: 'lib',
 
     // Enable stylistic formatting rules
